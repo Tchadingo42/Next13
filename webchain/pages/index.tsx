ab58f7c42e0b34f8c729313b2fa6a646320951da
Tchadingo42 <teichris@outlook.fr>
2022-11-16 18:41:37 +0100

    posts and exerpt part1

--- webchain/pages/index.tsx
+++ webchain/pages/index.tsx
@@ -2,15 +2,39 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+const posts = 
+[
+  { title: "L'instabilité des cryptos ", exerpt: 'Comprendre le market'},
+  { title: "La vérité du E-commerce ", exerpt: 'Info et Intox'},
+];
+
+
+
+  const Home: NextPage = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Create Next App</title>
+        <title>Webchain</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    </div>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => ( 
+          <div>
+          {post.title}
+          {post.exerpt}
+        </div>
+        ))}
+      </div >
+      
+      <div className='lg:col-span-4 col-span-1' >
+        <div className='lg:sticky relative top-8' >
+
+        </div>
+      </div>
+      </div>
+      </div>
   )
 }
 
